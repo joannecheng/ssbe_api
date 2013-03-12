1f792c8363df59c0000ba96854b09d88b1abb49c
Joanne Cheng <chengjoanne2@gmail.com>
2013-03-12 15:39:09 -0600

    first passing spec

--- app/helpers/ssbe_api/docs_helper.rb
+++ app/helpers/ssbe_api/docs_helper.rb
@@ -2,7 +2,16 @@ module SsbeApi
   module DocsHelper
 
     def group_routes_by_controller
-      named_routes = Rails.application.routes.routes.named_routes
+      Rails.application.routes.routes.named_routes.map do |k, v| 
+        v.defaults.merge({
+          :request_method => v.constraints[:request_method],
+          :path => v.path.spec
+        })
+      end.group_by { |path| path[:controller] }
+    end
+
+    def controllers
+      Rails.application.routes.routes.named_routes.map { |k, v| v.defaults[:controller] }.compact.uniq
     end
   end
 end
--- spec/integration/docs_spec.rb
+++ spec/integration/docs_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 describe "docs/index.html" do
   it 'lists out routes' do
     visit docs_path
-    within '#resources_container' do
-      page.should have_content "GET /clients"
+    within '#resources-container' do
+      page.should have_content "/clients"
     end
   end
 end
