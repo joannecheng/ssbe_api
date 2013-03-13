1845e309393bd5de0cf498358d0f3b9049712e32
Joanne Cheng <chengjoanne2@gmail.com>
2013-03-13 09:50:07 -0600

    ignore paths with empty request methods

--- app/helpers/ssbe_api/docs_helper.rb
+++ app/helpers/ssbe_api/docs_helper.rb
@@ -7,15 +7,12 @@ module SsbeApi
           :request_method => format_verb(v.verb),
           :path => v.path.spec
         })
-      end.compact.group_by { |path| path[:controller] }
+      end.reject { |path| path[:request_method].empty? }.group_by { |path| path[:controller] }
     end
 
     def format_verb(verb)
       verb.inspect.gsub(/\W+/, '')
     end
 
-    def controllers
-      Rails.application.routes.routes.named_routes.map { |k, v| v.defaults[:controller] }.compact.uniq
-    end
   end
 end
